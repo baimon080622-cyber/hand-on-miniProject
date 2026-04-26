ce0351c3bb129a59548ac8ec2f6f04d212c46752
Korraphat Tadyoo <baimon080622@email.com>
2026-04-26 16:21:23 +0700

    Javascript

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 //
 // Variables
 //
+const list = document.getElementById("list")
+const showGPAResult = document.getElementById("show-gpa-result");
+const showHistory   = document.getElementById("show-history");
+const buttonAdd     = document.getElementById("buttonAdd");
+const buttonReset   = document.getElementById("buttonReset");
+const buttonCal     = document.getElementById("buttonCal");
 
+console.log(list)
 
+console.log(buttonAdd)
 //
 // Methods
 //
 
+function handleReset() {
+    console.log("handle Reset")
 
+}
+function handleCal(){
+    console.log("handle Cal")
+}
+
+function handleAdd() {
+    console.log("handle Add")
+}
 //
 // Inits & Event Listeners
 //
+
+buttonAdd.addEventListener("click" , handleAdd)
+buttonReset.addEventListener("click",handleReset)
+buttonCal.addEventListener("click", handleCal)
